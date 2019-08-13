6b565afe696c63b047eb9167817d0262f811e400
Aleksandr Klimenko <an.klimenko>
2019-08-13 15:26:14 +0300

    output function ib 2nd task

--- script.js
+++ script.js
@@ -37,30 +37,30 @@ async function getUserWithTasksAmount() {
 }
 
 
-getUserWithTasksAmount()
-  .then(value => {
-    const usersCount = [];
+function renderTodos(arrOfTodos) {
+  const usersCount = [];
  
-    for (let task of value) {
-       if (!usersCount.includes(task.userId)) {
-        usersCount.push(task.userId);
-      }
+  for (let task of arrOfTodos) {
+     if (!usersCount.includes(task.userId)) {
+      usersCount.push(task.userId);
     }
+  }
 
-    for (let userIndex of usersCount) {
-      const completedTasks = (value.filter (task => task.userId === userIndex && task.completed)).length;
-      const uncompletedTasks = (value.filter (task => task.userId === userIndex && !task.completed)).length;
+  for (let userIndex of usersCount) {
+    const completedTasks = (arrOfTodos.filter (task => task.userId === userIndex && task.completed)).length;
+    const uncompletedTasks = (arrOfTodos.filter (task => task.userId === userIndex && !task.completed)).length;
 
-      const li = document.createElement('li');
+    const li = document.createElement('li');
 
-      li.textContent = `Пользователь userID=${userIndex} имеет ${completedTasks} завершенных и ${uncompletedTasks} незавершенных заданий`;
-      userList.appendChild(li);
-    }
-  })
+    li.textContent = `Пользователь userID=${userIndex} имеет ${completedTasks} завершенных и ${uncompletedTasks} незавершенных заданий`;
+    userList.appendChild(li);
+  }
+}
+
+getUserWithTasksAmount()
+  .then(value => renderTodos(value))
   .catch(err => console.log(err))
 
 //2nd task end///////////////////////////////////////////////////////////////////
 
 //3d task end - https://codepen.io/alexklimenko/pen/dybYjpZ
-
-
